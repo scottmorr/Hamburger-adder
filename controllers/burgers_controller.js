0a169b79cd6fff73d83aa8bfc3eb1e52e6b8a2f1
Scott Morrison <scottmorr75@gmail.com>
2020-05-20 10:47:12 -0600

    aded handelbars and router

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,8 @@
 // * Express
 // * `burger.js`
 //Create the `router` for the app, and export the `router` at the end of your file.
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
+var router = express.Router()
 
 var PORT = process.env.PORT || 3000;
 
 var app = express();
-
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -11,13 +12,10 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);

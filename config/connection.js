8253d5f25515ac533662cb361a531ecf8be1ed11
Scott Morrison <scottmorr75@gmail.com>
2020-05-30 17:52:01 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(JSON.stringify(process.env.JAWS_URL));
+  connection = mysql.createConnection("mysql://aki85jh87n2k7h48:vdsvsep81n7dhydr@ocvwlym0zv3tcn68.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vhyuxsvbo206ehg5");
 }else {
   var connection = mysql.createConnection({
     host: "localhost",
